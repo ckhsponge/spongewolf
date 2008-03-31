08706726d9e718b332ece108a42a495127b8010f
ckhsponge <chris@spongecell.com>
2008-03-31 00:07:27 -0700

    no fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,15 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
-require 'test_help'
+#require 'test_help'
+
+require 'test/unit'
+require 'active_support/test_case'
+#require 'active_record/fixtures'
+require 'action_controller/test_case'
+require 'action_controller/test_process'
+require 'action_controller/integration'
+require 'action_mailer/test_case' if defined?(ActionMailer)
+
 
 class Test::Unit::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
@@ -19,20 +28,23 @@ class Test::Unit::TestCase
   # The only drawback to using transactional fixtures is when you actually 
   # need to test transactions.  Since your test is bracketed by a transaction,
   # any transactions started in your code will be automatically rolled back.
-  self.use_transactional_fixtures = true
+  #self.use_transactional_fixtures = false
 
   # Instantiated fixtures are slow, but give you @david where otherwise you
   # would need people(:david).  If you don't want to migrate your existing
   # test cases which use the @david style and don't mind the speed hit (each
   # instantiated fixtures translates to a database query per test method),
   # then set this back to true.
-  self.use_instantiated_fixtures  = false
+  #self.use_instantiated_fixtures  = false
 
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
-  fixtures :all
+  #fixtures :all
+  
+  def load_fixtures
+  end
 
   # Add more helper methods to be used by all tests here...
 end
