e429f1b6d8498f7835f770af6b32ee3ad0836079
ckhsponge <chris@spongecell.com>
2008-04-14 09:33:25 -0700

    host

--- vendor/plugins/sponger/lib/sponger/resource.rb
+++ vendor/plugins/sponger/lib/sponger/resource.rb
@@ -1,7 +1,7 @@
 class Sponger::Resource < ActiveResource::Base
-  #HOST = "http://spongecell.com"
+  HOST = "http://spongecell.com"
   #HOST = "http://calf.spongecell.com"
-  HOST = "http://127.0.0.1:3001"
+  #HOST = "http://127.0.0.1:3001"
   self.site = "#{HOST}/api"
   
   @@token = nil
